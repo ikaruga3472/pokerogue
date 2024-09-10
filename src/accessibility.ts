7b86f0e570e14a08d2326cc8365cf385a0b5f977
Ikaruga <kussbyungyun@gmail.com>
2024-09-10 21:51:37 +0900

    수정함

--- src/accessibility.ts
+++ src/accessibility.ts
@@ -3,34 +3,33 @@ export default class PRAccess {
   private ariaH2: HTMLElement;
   private textList: string[];
   private forceChange: boolean;
-  // a
+
   // 싱글톤
-  private static instance: PRAccess
+  private static instance: PRAccess;
   private constructor () {
   }
   public static getInstance () {
-      return this.instance || (this.instance = new this())
+    return this.instance || (this.instance = new this());
   }
   setup () {
-    this.accessdiv = document.createElement('div');
-    this.accessdiv.style.height = '0px';
-    this.accessdiv.style.width = '0px';
-    this.accessdiv.style.overflow = 'hidden';
-    this.accessdiv.classList.add('ariadiv');
-    this.ariaH2 = document.createElement('h2');
-    this.accessdiv.ariaLive = 'polite';
+    this.accessdiv = document.createElement("div");
+    this.accessdiv.style.height = "0px";
+    this.accessdiv.style.width = "0px";
+    this.accessdiv.style.overflow = "hidden";
+    this.accessdiv.classList.add("ariadiv");
+    this.ariaH2 = document.createElement("h2");
+    this.accessdiv.ariaLive = "polite";
     this.accessdiv.appendChild(this.ariaH2);
     document.body.append(this.accessdiv);
     console.log("PR Access setup complete.");
   }
-  
+
   speak(text) {
     //console.log(text);
-    if (text == this.ariaH2.innerHTML) {
-      console.log("겹치는 문자 감지")
+    if (text === this.ariaH2.innerHTML) {
+      console.log("겹치는 문자 감지");
       return;
-    }
-    else {
+    } else {
       this.ariaH2.innerHTML = text;
     }
   }
